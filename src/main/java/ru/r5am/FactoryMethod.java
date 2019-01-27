ee741a96c95bd8052ea161b58cdba9ff1dbc302b
Zoer <richmanfx@yandex.ru>
2019-01-27 21:12:09 +0300

    Реализован пример шаблона Фабричный метод.

--- src/main/java/ru/r5am/FactoryMethod.java
+++ src/main/java/ru/r5am/FactoryMethod.java
@@ -1,20 +1,81 @@
 package ru.r5am;
 
-import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
 /**
  * Created by Zoer on 20.12.2016.
  * Пример реализации паттерна Фабрика (FactoryMethod)
  *
- * -
  */
-
 class FactoryMethod {
-    // Создать логер
-    public static final Logger log = LogManager.getLogger(FactoryMethod.class);
+
+    private static final Logger log = LogManager.getLogger(FactoryMethod.class);
+
     public static void main(String[] args) {
-        System.out.println("Привет!");
-        log.info("Привет в лог!");
+
+        // Две разные страницы
+        BasePage page1 = new PageCreatorA().factoryMethod();
+        BasePage page2 = new PageCreatorB().factoryMethod();
+
+        // Остальной код работает с созданными страницами единообразно через общий интерфейс
+        log.info(String.format("%s, класс %s", page1.getPageName(), page1.getClass()));
+        log.info(String.format("%s, класс %s", page2.getPageName(), page2.getClass()));
+
+    }
+
+}
+
+/**
+ * Общий интерфейс всех страниц
+ */
+interface BasePage {
+    String getPageName();
+}
+
+/**
+ * Реализация страницы A
+ */
+class PageA implements BasePage {
+    // Реализация метода интерфейса
+    public String getPageName() {
+        return "Это страница PageA";
+    }
+}
+
+/**
+ * Реализация страницы B
+ */
+class PageB implements BasePage {
+    // Реализация метода интерфейса
+    public String getPageName() {
+        return "Это страница PageB";
+    }
+}
+
+/**
+ * Базовый класс фабрики с переопределяемым при создании объекта методом
+ */
+abstract class Creator {
+    public abstract BasePage factoryMethod();
+}
+
+/**
+ * Конкретный класс страницы A
+ */
+class PageCreatorA extends Creator {
+    @Override
+    public BasePage factoryMethod() {
+        return new PageA();
+    }
+}
+
+/**
+ * Конкретный класс станицы B
+ */
+class PageCreatorB extends Creator {
+    @Override
+    public BasePage factoryMethod() {
+        return new PageB();
     }
 }
